317eed1f9c44b78b85607538a060c69366369555
Milagros <milagroschaconies@gmail.com>
2024-05-13 23:33:32 -0300

    Agregar archivos de proyecto.

---
+++ frmFirma.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace pryEtapa1
+{
+    partial class frmFirma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctLogo = new System.Windows.Forms.PictureBox();
+            this.lblInstruccion = new System.Windows.Forms.Label();
+            this.pctFirma = new System.Windows.Forms.PictureBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctLogo
+            // 
+            this.pctLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pctLogo.Image = global::pryEtapa1.Properties.Resources.logo;
+            this.pctLogo.Location = new System.Drawing.Point(220, -11);
+            this.pctLogo.Name = "pctLogo";
+            this.pctLogo.Size = new System.Drawing.Size(120, 120);
+            this.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctLogo.TabIndex = 0;
+            this.pctLogo.TabStop = false;
+            // 
+            // lblInstruccion
+            // 
+            this.lblInstruccion.AutoSize = true;
+            this.lblInstruccion.BackColor = System.Drawing.Color.Transparent;
+            this.lblInstruccion.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruccion.Location = new System.Drawing.Point(53, 103);
+            this.lblInstruccion.Name = "lblInstruccion";
+            this.lblInstruccion.Size = new System.Drawing.Size(466, 42);
+            this.lblInstruccion.TabIndex = 1;
+            this.lblInstruccion.Text = "Por favor, dibuja tu firma con el mouse dentro del cuadrado \r\ny luego presiona Gu" +
+    "ardar.";
+            this.lblInstruccion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pctFirma
+            // 
+            this.pctFirma.BackColor = System.Drawing.Color.Transparent;
+            this.pctFirma.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pctFirma.Location = new System.Drawing.Point(114, 157);
+            this.pctFirma.Name = "pctFirma";
+            this.pctFirma.Size = new System.Drawing.Size(350, 350);
+            this.pctFirma.TabIndex = 2;
+            this.pctFirma.TabStop = false;
+            this.pctFirma.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseDown);
+            this.pctFirma.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseMove);
+            this.pctFirma.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseUp);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(345, 513);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(119, 27);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(221, 513);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(119, 27);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // frmFirma
+            // 
+            this.AcceptButton = this.btnGuardar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::pryEtapa1.Properties.Resources.frmInicio;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.pctFirma);
+            this.Controls.Add(this.lblInstruccion);
+            this.Controls.Add(this.pctLogo);
+            this.MaximizeBox = false;
+            this.Name = "frmFirma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dibujar firma";
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctLogo;
+        private System.Windows.Forms.Label lblInstruccion;
+        private System.Windows.Forms.PictureBox pctFirma;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ frmFirma.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEtapa1
+{
+    public partial class frmFirma : Form
+    {
+        // Variable para almacenar la posición anterior del mouse durante el dibujo
+        private Point puntoAnterior;
+        // Bandera para indicar si el mouse está actualmente dibujando
+        private bool isMouseDrawing = false;
+        // Bitmap utilizado para almacenar la firma del usuario
+        private Bitmap firmaBitmap;
+        public frmFirma()
+        {
+            InitializeComponent();
+            InitializeFirmaPictureBox();
+            // Eventos para los eventos MouseDown, MouseMove y MouseUp del PictureBox
+            pctFirma.MouseDown += new MouseEventHandler(pctFirma_MouseDown);
+            pctFirma.MouseMove += new MouseEventHandler(pctFirma_MouseMove);
+            pctFirma.MouseUp += new MouseEventHandler(pctFirma_MouseUp);
+        }
+        private void InitializeFirmaPictureBox()
+        {
+            // Crea un Bitmap del mismo tamaño que el PictureBox
+            firmaBitmap = new Bitmap(pctFirma.Width, pctFirma.Height);
+
+            // Asigna el Bitmap como imagen del PictureBox
+            pctFirma.Image = firmaBitmap;
+
+            // Obtiene un objeto Graphics para dibujar en el Bitmap
+            Graphics g = Graphics.FromImage(pctFirma.Image);
+
+            // Limpia el Bitmap con color blanco
+            g.Clear(Color.White);
+
+            // Libera los recursos utilizados por el objeto Graphics
+            g.Dispose();
+        }
+
+        private void pctFirma_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Verifica si se hizo clic con el botón izquierdo del mouse
+            if (e.Button == MouseButtons.Left)
+            {
+                // Indica que el mouse está comenzando a dibujar
+                isMouseDrawing = true;
+
+                // Almacena la posición inicial del clic
+                puntoAnterior = e.Location;
+            }
+        }
+
+        private void pctFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Verifica si el mouse está dibujando
+            if (isMouseDrawing)
+            {
+                // Crea un objeto Graphics temporal para dibujar en el Bitmap
+                using (Graphics g = Graphics.FromImage(pctFirma.Image))
+                {
+                    // Habilita el suavizado de líneas para mejorar la apariencia del trazo
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                    // Crea un lápiz negro de grosor 2 para dibujar
+                    using (Pen pen = new Pen(Color.Black, 2))
+                    {
+                        // Dibuja una línea desde la posición anterior del mouse hasta la posición actual
+                        g.DrawLine(pen, puntoAnterior, e.Location);
+                    }
+                }
+
+                // Actualiza la posición anterior del mouse para el siguiente movimiento
+                puntoAnterior = e.Location;
+
+                // Indica al PictureBox que necesita redibujarse
+                pctFirma.Invalidate();
+            }
+        }
+
+        private void pctFirma_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Indica que el mouse ya no está dibujando
+            isMouseDrawing = false;
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Obtiene la fecha y hora actual con formato específico para el nombre del archivo
+            string fecha = DateTime.Now.ToString("yyyy.MM.dd / HH:mm:ss") + ".png";
+
+            // Construye la ruta de la carpeta "FIRMA" en el escritorio del usuario
+            string carpeta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Firma");
+
+            // Crea la carpeta "FIRMA" si no existe
+            Directory.CreateDirectory(carpeta);
+
+            // Combina la fecha y la extensión ".png" para formar el nombre del archivo
+            string nombreArchivo = $"{fecha}.png";
+
+            // Construye la ruta completa del archivo en el disco
+            string rutaCompleta = Path.Combine(carpeta, nombreArchivo);
+
+            // Crea un nuevo Bitmap del mismo tamaño que el PictureBox
+            Bitmap bmp = new Bitmap(pctFirma.Width, pctFirma.Height);
+
+            // Dibuja el contenido del PictureBox en el bitmap
+            pctFirma.DrawToBitmap(bmp, pctFirma.ClientRectangle);
+
+            // Guarda el bitmap en un archivo
+            bmp.Save(rutaCompleta, ImageFormat.Png);
+
+            // Libera los recursos utilizados por el bitmap
+            bmp.Dispose();
+
+            // Muestra un mensaje al usuario indicando que la firma se guardó correctamente
+            MessageBox.Show("Firma guardada correctamente en escritorio.");
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            // Obtiene un objeto Graphics para dibujar en el Bitmap
+            using (Graphics g = Graphics.FromImage(firmaBitmap))
+            {
+                // Limpia el Bitmap con color blanco
+                g.Clear(Color.White);
+            }
+
+            // Invalida el PictureBox para que se actualice la imagen
+            pctFirma.Invalidate();
+        }
+    }
+}
